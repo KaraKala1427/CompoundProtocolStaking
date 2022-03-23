8b87602b51e45fd670525bff63e8e30f10eeb033
yernar <ernarerbol027@gmail.com>
2022-03-23 13:37:52 +0600

    compound staking in rinkeby network added

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Compound Staking project 
+
+To run the web app locally, use npm to install [http-server](https://www.npmjs.com/package/http-server), and run it from the project root directory.
+
+Rinkeby network smart contract address : 0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e
+
+ganache-cli -f "https://rinkeby.infura.io/v3/502b002c20104834a8fbda450229f913" -m "there mnemonic 12 words"
+
+After stake, you can see your cTokens in metamask by importing Token Contract Address as above of rinkeby. 
+
+Etherscan rinkeby where you can check : https://rinkeby.etherscan.io/address/0xd6801a1dffcd0a410336ef88def4320d6df1883e#readContract 
+
---
+++ app.js
@@ -0,0 +1,54 @@
+const compound = new Compound(window.ethereum);
+
+const ethSupplyInput = document.getElementById('eth-supply');
+const ethSupplyButton = document.getElementById('eth-supply-button');
+const ethRedeemInput = document.getElementById('eth-redeem');
+const ethRedeemButton = document.getElementById('eth-redeem-button');
+const enableEthereumButton = document.getElementById('enable-button');
+
+enableEthereumButton.onclick = async () => {
+  await ethereum.request({ method: 'eth_requestAccounts' });
+};
+
+ethSupplyButton.onclick = async () => {
+  const amount = +ethSupplyInput.value;
+  await supply(Compound.ETH, amount);
+};
+
+ethRedeemButton.onclick = async () => {
+  const amount = +ethRedeemInput.value;
+  await redeem(Compound.cETH, amount);
+};
+
+async function stake(asset, amount) {
+  if (!isNaN(amount) && amount !== 0) {
+    try {
+      const trx = await compound.supply(asset, amount);
+      console.log(asset, 'Supply', amount, trx);
+      console.log('Transaction Hash', trx.hash);
+    } catch (err) {
+        console.log(err);
+      alert('ошибка при stake: ' + JSON.stringify(err));
+    }
+  }
+  else{
+      alert('Введите количество ETH которую хотите застейкать')
+  }
+}
+
+async function unstake(asset, amount) {
+  if (!isNaN(amount) && amount !== 0) {
+    try {
+      const trx = await compound.redeem(asset, amount);
+      console.log(asset, 'Redeem', amount, trx);
+      console.log('Transaction Hash', trx.hash);
+    } catch (err) {
+        console.log(err);
+      alert(JSON.stringify(err));
+    }
+  }
+  else{
+    alert('Введите количество cETH которую хотите анстейкать')
+}
+}
+
